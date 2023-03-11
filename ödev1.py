14bb29b252b93178b9e868089d353edb738633b0
Uğur Şen <56ugurcan@gmail.com>
2023-03-11 18:46:52 +0300

    1 numaralı ödevin eklenmesi

---
+++ ödev1.py
@@ -0,0 +1,49 @@
+
+# 1-) Python'da Veri Tiplerini araştırınız, her bir veri tipi için kendi cümlelerinizle açıklamalar yazınız.
+
+         # Metin tipleri: str 
+            # Girilen verinin string(metinsel) bir ifade olduğunu belirtir. Genellikle metinsel veri tipleri string değişkeni içerisinde tutulur.
+
+        # Sayısal tipler: int , float
+            # Gerilen verilerin sayısal tipte olduğunu belirtir. int veri tipi tam sayıları ifade eder. float ondalıklı sayılırı ifade eder.
+
+        # Dizi tipleri:  list , tuple ,set, dictionary
+            # List; sıralı ve elemanları değiştirilebilir bir dizidir. Dizi elemanlarının tekrarlamasına izin verir. Aynı elemandan aynı dizide birden fazla olabilir.
+            # Tuple; sıralı ve elemanları değiştirilemeyen bir dizidir. Dizi elemanlarının tekrarlamasına izin verir. Aynı elemandan aynı dizide birden fazla olabilir.
+            # Set; sırasız ve indexlenmemiş bir dizidir. Dizi elemanlarının tekrarlamasına izin vermez. Bir elemandan bir dizide bir tane olur.
+            # Dictionary; sırasız, elemanları değiştirilebilir ve indexlenmiş bir dizidir. Dizi elemanlarının tekrarlamasına izin vermez. Bir elemandan dizide bir tane olur.
+
+        # Adresleme tipleri: dict
+               #dict veri tipinde key ve value olarak iki değişken kısımdan oluşur. dict veri tiplerine erişim key değeri üzerinden olur.
+
+          # Mantıksal tipler: bool
+            #Mantıksal veri tipi boolean mantıksal veriler olan true(doğru) veya false(yanlış) değerlerini kaydetmek için kullanılır.
+
+        #Küme tipleri: set,frozenset
+            # set:Liste, sözlük ve demet veri türü gibi birden çok veri türünü birlikte barındıran veri tipidir.
+            # frozenset:kısıtlanmış küme demektir.
+
+        # Binary tipler:Bytes,bytearray,memoryview,
+            # Bytes:tam sayıları tuttuğumuz 8 bitlik veri depolayan bir değer tipidir.örn b"merhaba" 
+            # Bytearray:bytes veri tipinin aksine, üzerinde değişiklik yapılabilen bir veri tipidir.
+            # Memoryview:Python’da arabellek protokolünü ortaya çıkarmanın güvenli bir yolu bellek görüntüleme nesnesi oluşturmaktır.
+
+
+
+# 2-) Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz.
+
+    # metinsel veri tipine örnek olarak: giriş bilgileri, ödevler kısmında ödev tanımının yapıldığı alan örnek olarak gösterile bilir  verile biilir
+    # sayısal veri tiplerine ise örenk verecek olursak kurusn tammalanma yüzdesi kulanıcı şifresi vs sayısal veri tipleri örneklerindendir.
+
+
+
+# 3-) Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz
+
+        # kullanıcı giriş bilgilerinin kontrol edildiği alanda şart bloğu kullanılmış olabilir.
+        # db_Kullanıcı = "uğurcanşen"
+        # kullanıcı adı = input('Kulalnıcı adınızı girin: ')
+            # if kullanıcı adı == db_Kullanıcı:
+                # print("Giriş işleminiz başarılı")
+            # else:
+            #     print("Kullanıcı adı hatalı")
+        
